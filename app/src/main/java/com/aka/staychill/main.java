d7f5d41ab1fe9773966cbfef457d59320bd9e2d1
AlonsoGG29 <gomez.alonso07@gmail.com>
2024-12-22 15:17:18 +0100

    Version 2 de login.xml

--- app/src/main/java/com/aka/staychill/main.java
+++ app/src/main/java/com/aka/staychill/main.java
@@ -1,9 +1,6 @@
 package com.aka.staychill;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
-
-    Button btnEntrar;
+public class main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-
-        btnEntrar = findViewById(R.id.btn_login);
-
-        //Funcion del boton de Entrar
-        btnEntrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(login.this, main.class);
-                startActivity(intent);
-            }
-        });
-
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
