4d11294dc8a5ef36ee18aca25cacc5dec2e5cf4d
AlonsoGG29 <gomez.alonso07@gmail.com>
2024-12-22 15:31:13 +0100

    Version 1 de signup.xml

--- app/src/main/java/com/aka/staychill/signup.java
+++ app/src/main/java/com/aka/staychill/signup.java
@@ -1,6 +1,9 @@
 package com.aka.staychill;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class signup extends AppCompatActivity {
 
+    Button btn_entrarSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
+
+        btn_entrarSignUp = findViewById(R.id.btn_signup);
+
+        //Funcion de boton "Entrar"
+        btn_entrarSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signup.this, main.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
