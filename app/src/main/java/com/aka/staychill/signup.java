0d73c08e3138047dd4e8ffbb501ae568a684bb50
am8DAM2 <angelcastillo4703@outlook.com>
2025-01-10 11:54:23 +0100

    Arreglo de la app

--- app/src/main/java/com/aka/staychill/signup.java
+++ app/src/main/java/com/aka/staychill/signup.java
@@ -1,6 +1,11 @@
 package com.aka.staychill;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +15,47 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class signup extends AppCompatActivity {
 
+    Button btn_entrarSignUp;
+    ImageView back_sign;
+    TextView inicio;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
+
+        btn_entrarSignUp = findViewById(R.id.btn_signup);
+        back_sign = findViewById(R.id.volver_sign);
+        inicio = findViewById(R.id.inicio_text);
+
+        // Funcion de boton "Entrar"
+        btn_entrarSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signup.this, main.class);
+                startActivity(intent);
+            }
+        });
+
+        //Funcion de texto si tienes cuenta
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signup.this, login.class);
+                startActivity(intent);
+            }
+        });
+
+        // Funcion de flecha
+        back_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finaliza esta actividad para volver a la anterior
+                finish();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/aka/staychill/splash.java
+++ app/src/main/java/com/aka/staychill/splash.java
@@ -1,24 +1,34 @@
 package com.aka.staychill;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.airbnb.lottie.LottieAnimationView;
 
 public class splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Inicializar la animación Lottie
+        LottieAnimationView lottieAnimationView = findViewById(R.id.lottieAnimationView);
+
+        // Aquí, puedes agregar un Listener o simplemente hacer que la animación se ejecute
+        lottieAnimationView.playAnimation();  // Esto arranca la animación
+
+        // Iniciar la actividad principal después de la animación
+        lottieAnimationView.addAnimatorListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                // Después de la animación, pasa a la actividad principal
+                startActivity(new Intent(splash.this, welcome.class));
+                finish(); // Para cerrar la actividad splash
+            }
         });
     }
 }
