2fe93ffbf7f4231801a2f869c60a8f0d11b71d00
Konrad_Alonso_Interfaces <rayo291066@gmail.com>
2025-01-15 13:54:07 +0100

    Botones cambiados

--- app/src/main/java/com/aka/staychill/splash.java
+++ app/src/main/java/com/aka/staychill/splash.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import com.airbnb.lottie.LottieAnimationView;
 
@@ -20,15 +21,14 @@ public class splash extends AppCompatActivity {
         // Aquí, puedes agregar un Listener o simplemente hacer que la animación se ejecute
         lottieAnimationView.playAnimation();  // Esto arranca la animación
 
-        // Iniciar la actividad principal después de la animación
-        lottieAnimationView.addAnimatorListener(new AnimatorListenerAdapter() {
+        // Usamos un Handler para iniciar la actividad después de la duración de la animación
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                // Después de la animación, pasa a la actividad principal
+            public void run() {
+                // Después de la animación (3 segundos o lo que definas), pasa a la actividad principal
                 startActivity(new Intent(splash.this, welcome.class));
                 finish(); // Para cerrar la actividad splash
             }
-        });
+        }, 4000); // Esto indica que el cambio de actividad ocurrirá después de 3 segundos
     }
 }
