6c778811356d16b01b860b2f90f64528c4d25181
am8DAM2 <angelcastillo4703@outlook.com>
2025-02-17 14:02:26 +0100

    Creando Evento

--- app/src/main/java/com/aka/staychill/Evento.java
+++ app/src/main/java/com/aka/staychill/Evento.java
@@ -1,44 +1,30 @@
 package com.aka.staychill;
 
+import java.time.OffsetTime;
+import java.util.Date;
+
 public class Evento {
 
-    //Clase que se comunica con MiAdaptador y le pasa la información
-
-    private final String imageUrl;
-    private final String text;
-    private final String imageUrl2;
+    private final String titulo;
     private final String nombre;
     private final String localizacion;
     private final String descripcion;
-    private final String fecha;
-    private final String hora;
+    private final Date fecha; // Puedes cambiarlo a LocalDate si es mejor para ti
+    private final OffsetTime hora; // Ahora maneja timetz correctamente
 
-    // Constructor para los nuevos campos
-    public Evento(String nombre, String localizacion, String descripcion, String fecha, String hora) {
+    public Evento(String nombre, String localizacion, String descripcion, Date fecha, OffsetTime hora) {
         this.nombre = nombre;
         this.localizacion = localizacion;
         this.descripcion = descripcion;
         this.fecha = fecha;
         this.hora = hora;
-        this.imageUrl = null;
-        this.text = null;
-        this.imageUrl2 = null;
+        this.titulo = null;
     }
 
-    // Constructor para los antiguos campos
-    public Evento(String imageUrl, String text, String imageUrl2) {
-        this.imageUrl = imageUrl;
-        this.text = text;
-        this.imageUrl2 = imageUrl2;
-        this.nombre = null;
-        this.localizacion = null;
-        this.descripcion = null;
-        this.fecha = null;
-        this.hora = null;
+    public OffsetTime getHora() {
+        return hora;
     }
 
-
-    // Getters
     public String getNombre() {
         return nombre;
     }
@@ -51,23 +37,13 @@ public class Evento {
         return descripcion;
     }
 
-    public String getFecha() {
+    public Date getFecha() {
         return fecha;
     }
 
-    public String getHora() {
-        return hora;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
     public String getText() {
-        return text;
+        return titulo;
     }
 
-    public String getImageUrl2() {
-        return imageUrl2;
-    }
+
 }
